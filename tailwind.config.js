ea444e9c87ca8f580f0a66de6f72699df3d466b9
Oisangui <ialoyola@uc.cl>
2020-12-16 19:28:34 -0300

    more or less

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'economist-red': '#e3120b',
+      }
+    },
   },
   variants: {
     extend: {},
--- templates/main.tsx
+++ templates/main.tsx
@@ -4,7 +4,6 @@ import classNames from "classnames";
 
 import "../styles/tailwind.css";
 
-import background from "../static/background.jpg";
 import logo from "../static/logo.svg";
 
 function Layer({ className, ...props}: React.ComponentPropsWithoutRef<"div">) {
@@ -14,23 +13,28 @@ function Layer({ className, ...props}: React.ComponentPropsWithoutRef<"div">) {
 // Make sure to 'export default' a React component
 export default function MainTemplate({ variables }: TemplateProps) {
   const {
+    quoteTheme = 'The World In 2021',
+    author =  'Michael Tubbs',
+    authorPosition = 'Mayor of Stockton',
     title = "Hello world Ignacio!",
     quote = "There is nothing inherently dignified in working two or three jobs, yet still being unable to afford the basics",
-    img = background,
     description = 'This is a description',
   } = variables;
 
   return (
     <>
-      
-      <Layer className="bg-red-600" />
-      <Layer className="flex flex-col justify-end items-start px-12 py-12 text-white">
+      <Layer className="bg-red-600 rounded-lg" />
+      <Layer className="flex flex-col justify-start items-start px-20 py-12 text-white">
+        <h1 className="text-2xl mt-4 font-medium">{quoteTheme}</h1>
+      </Layer>
+      <Layer className="flex flex-col justify-center items-start px-40 py-12 text-white">
+        <h2 className="text-5xl mt-10 font-serif">{quote}</h2>
+        <h2 className="text-1xl mt-4 font-semibold">{author}</h2>
+        <h2 className="text-1xl mt-4">{authorPosition}</h2>
+        
+      </Layer>
+      <Layer className="flex flex-col justify-end items-end px-20 py-12 text-white">
         <img src={logo} className={classNames("filter-white" /* custom */, "w-36 h-36")} />
-        <h1 className="text-6xl mt-4">{title}</h1>
-        <h2 className="text-4xl mt-4">{quote}</h2>
-        {description && (
-          <h2 className="text-4xl font-mono tracking-wider mt-2">{description}</h2>
-        )}
       </Layer>
     </>
   );
