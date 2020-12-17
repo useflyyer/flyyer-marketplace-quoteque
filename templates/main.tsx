dd995e82119099dbca98de5207bf676362c80dc1
Patricio López Juri <lopezjuripatricio@gmail.com>
2020-12-17 09:20:44 -0300

    feat: Custom whatsapp view

--- templates/main.tsx
+++ templates/main.tsx
@@ -10,8 +10,19 @@ function Layer({ className, ...props}: React.ComponentPropsWithoutRef<"div">) {
   return <div {...props} className={classNames("absolute inset-0", className)} />;
 }
 
+const Logo = ({ className, ...props }: React.ComponentPropsWithoutRef<"img">) => (
+  <img src={logo} className={classNames("filter-white" /* custom */, "w-36 h-36", "object-contain", className)} {...props} />
+)
+
+const SVGQuote = (props: React.ComponentPropsWithoutRef<"svg">) => (
+  <svg className="mr-4" width="82" height="49" viewBox="0 0 82 49" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M22.6639 0H38.1428C22.9274 21.1215 22.929 31.899 38.1428 49H0.305414C-1.32435 27.3131 3.20851 16.4951 22.6639 0Z" fill="white"/>
+    <path d="M66.5211 0H82C66.7846 21.1215 66.7862 31.899 82 49H44.1626C42.5329 27.3131 47.0657 16.4951 66.5211 0Z" fill="white"/>
+  </svg>
+)
+
 // Make sure to 'export default' a React component
-export default function MainTemplate({ variables }: TemplateProps) {
+export default function MainTemplate({ agent, variables }: TemplateProps) {
   const {
     quoteTheme = 'The World In 2021',
     author =  'Ignacio Loyola',
@@ -19,16 +30,22 @@ export default function MainTemplate({ variables }: TemplateProps) {
     quote = "There are many bad things that are bad for the eyes, but none are as bad as not washing your hands after eating something spicy.",
   } = variables;
 
+  if (agent.name === "whatsapp") {
+    return (
+      <Layer className="flex flex-col justify-start items-stretch px-16 py-12 text-white bg-red-600">
+        <SVGQuote className="w-72 h-72" />
+        <Logo className="w-72 h-72 self-end" />
+      </Layer>
+    )
+  }
+
   return (
     <Layer className="flex flex-col justify-start items-stretch px-16 py-12 text-white bg-red-600">
       <h1 className="text-2xl mt-1 font-semibold tracking-wide">{quoteTheme}</h1>
 
       <div className="flex-grow flex flex-row justify-start items-stretch mt-10">
         <div>
-          <svg className="mr-4" width="82" height="49" viewBox="0 0 82 49" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M22.6639 0H38.1428C22.9274 21.1215 22.929 31.899 38.1428 49H0.305414C-1.32435 27.3131 3.20851 16.4951 22.6639 0Z" fill="white"/>
-            <path d="M66.5211 0H82C66.7846 21.1215 66.7862 31.899 82 49H44.1626C42.5329 27.3131 47.0657 16.4951 66.5211 0Z" fill="white"/>
-          </svg>
+          <SVGQuote />
         </div>
 
         <div className="flex flex-col justify-between">
@@ -48,7 +65,7 @@ export default function MainTemplate({ variables }: TemplateProps) {
         </div>
 
         <div className="flex flex-col justify-end flex-shrink-0">
-          <img src={logo} className={classNames("filter-white" /* custom */, "w-36 h-36", "object-contain")} />
+          <Logo />
         </div>
       </div>
     </Layer>
