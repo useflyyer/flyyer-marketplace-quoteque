4d99a176fc86f2b935c04553da994859b7322e36
Patricio López Juri <lopezjuripatricio@gmail.com>
2020-12-17 09:16:47 -0300

    fix: Use semantic html

--- templates/main.tsx
+++ templates/main.tsx
@@ -20,30 +20,37 @@ export default function MainTemplate({ variables }: TemplateProps) {
   } = variables;
 
   return (
-    <Layer className="bg-red-600" >
-      <Layer className="flex flex-col justify-start items-start px-16 py-12 text-white">
-        <h1 className="text-2xl mt-1 font-semibold tracking-wide">{quoteTheme}</h1>
-      </Layer>
-      <Layer>
-        <h1 style={{fontFamily: "Times New Roman"}} className='pt-24 pl-12 text-9xl font-serif font-medium text-white'>
-          {`"`}
-        </h1>
-      </Layer>
-      <Layer className="flex flex-col justify-start items-start pl-24 lg:pl-32 pt-28 lg:py-20 text-white pr-32 lg:pr-80 tracking-normal">
-        <h2 className="text-2xl lg:text-4xl mt-1 lg:mt-10 font-serif font-medium">{quote}</h2>
-      </Layer>
-      <Layer className="flex flex-col justify-end items-start pl-32 py-10 text-white pr-80 tracking-normal">
+    <Layer className="flex flex-col justify-start items-stretch px-16 py-12 text-white bg-red-600">
+      <h1 className="text-2xl mt-1 font-semibold tracking-wide">{quoteTheme}</h1>
+
+      <div className="flex-grow flex flex-row justify-start items-stretch mt-10">
         <div>
-          <svg width="5rem" height="5rem">
-            <rect x="0rem" y="4.5rem" width="3rem" height="0.35rem" fill="white"></rect>
+          <svg className="mr-4" width="82" height="49" viewBox="0 0 82 49" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M22.6639 0H38.1428C22.9274 21.1215 22.929 31.899 38.1428 49H0.305414C-1.32435 27.3131 3.20851 16.4951 22.6639 0Z" fill="white"/>
+            <path d="M66.5211 0H82C66.7846 21.1215 66.7862 31.899 82 49H44.1626C42.5329 27.3131 47.0657 16.4951 66.5211 0Z" fill="white"/>
           </svg>
         </div>
-        <h2 className="text-2xl mt-1 font-semibold">{author}</h2>
-        <h2 className="text-2xl mt-1 font-light">{authorPosition}</h2>
-      </Layer>
-      <Layer className="flex flex-col justify-end items-end px-20 py-12 text-white">
-        <img src={logo} className={classNames("filter-white" /* custom */, "w-36 h-36")} />
-      </Layer>
+
+        <div className="flex flex-col justify-between">
+          <blockquote className="text-2xl lg:text-5xl leading-snug lg:leading-snug font-serif font-medium">
+            {quote}
+          </blockquote>
+
+          <div>
+            <div>
+              <svg width="5rem" height="5rem">
+                <rect x="0rem" y="4.5rem" width="3rem" height="0.35rem" fill="white"></rect>
+              </svg>
+            </div>
+            <p className="text-2xl mt-1 font-semibold">{author}</p>
+            <p className="text-2xl mt-1 font-light">{authorPosition}</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col justify-end flex-shrink-0">
+          <img src={logo} className={classNames("filter-white" /* custom */, "w-36 h-36", "object-contain")} />
+        </div>
+      </div>
     </Layer>
   );
 }
